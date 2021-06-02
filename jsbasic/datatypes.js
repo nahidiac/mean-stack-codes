fd7310156860378e62158aa67a8435e71fa9b9fd
Komol Nath Roy <komol.roy@selise.ch>
2021-06-02 20:47:50 +0600

    datatype.js created

---
+++ jsbasic/datatypes.js
@@ -0,0 +1,40 @@
+let user = 'John Doe';
+let age = 25;
+let message = 'Welcome to the course';
+
+// big integer
+// adding n after any number will make it consider as big integer
+const bigInt = 1872182718271827182781271827182718271827182712n;
+console.log(bigInt);
+
+console.log(typeof bigInt);
+
+let user_datatype = typeof (user);
+console.log(user_datatype);
+
+let age_datatype = typeof (age);
+console.log(age_datatype);
+
+let message_datatype = typeof (message);
+console.log(message_datatype);
+
+console.log(user);
+console.log(age);
+console.log(message);
+
+
+// data type checking
+
+let name = null;
+console.log(name);
+console.log(typeof name)
+name = true;
+console.log(typeof name)
+let testNumber = 25.5;
+console.log(typeof testNumber);
+name = undefined;
+console.log(typeof name)
+// typeof 0
+// typeof 0n
+//alert()
+console.log(typeof alert)
